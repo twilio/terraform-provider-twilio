c73fee46ef9242440d10cd77ccdd25e6a09f3e6d
Sam Harrison <sharrison@twilio.com>
2020-10-23 12:02:06 -0500

    checkpointing latest work

--- go.mod
+++ go.mod
@@ -1,13 +1,10 @@
 module github.com/twilio/terraform-provider-twilio
 
-go 1.13
+go 1.15
+
+replace github.com/twilio/twilio-go => /Users/sharrison/go/src/github.com/twilio/twilio-go
 
 require (
-	github.com/hashicorp/go-getter v1.4.2-0.20200106182914-9813cbd4eb02 // indirect
-	github.com/hashicorp/hcl/v2 v2.3.0 // indirect
-	github.com/hashicorp/terraform-config-inspect v0.0.0-20191212124732-c6ae6269b9d7 // indirect
-	github.com/hashicorp/terraform-plugin-sdk v1.7.0
-	github.com/stretchr/testify v1.4.0 // indirect
-	github.com/twilio/twilio-go v0.0.1
-	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.4
+	github.com/twilio/twilio-go v0.0.0-00010101000000-000000000000
 )
--- twilio/provider.go
+++ twilio/provider.go
@@ -2,8 +2,10 @@ package twilio
 
 import (
 	"context"
-	"github.com/hashicorp/terraform-plugin-sdk/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	twilio "github.com/twilio/terraform-provider-twilio/client"
+	studioV2 "github.com/twilio/terraform-provider-twilio/studio/v2"
 	client "github.com/twilio/twilio-go/twilio"
 )
 
@@ -25,24 +27,9 @@ func Provider() *schema.Provider {
 			},
 		},
 		DataSourcesMap: map[string]*schema.Resource{
-			"twilio_available_phone_numbers_local": dataSourceAvailablePhoneNumbersLocal(),
-			"twilio_taskrouter_taskqueues":         dataSourceTaskRouterTaskQueues(),
-			"twilio_taskrouter_workflows":          dataSourceTaskRouterTaskQueues(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
-			"twilio_chat_service":          resourceChatService(),
-			"twilio_incoming_phone_number": resourceIncomingPhoneNumber(),
-			"twilio_proxy_service":         resourceProxyService(),
-			"twilio_proxy_phone_number":    resourceProxyPhoneNumber(),
-			"twilio_studio_flow":           resourceStudioFlow(),
-			"twilio_sync_service":          resourceSyncService(),
-			"twilio_taskrouter_workspace":  resourceTaskRouterWorkspace(),
-			"twilio_taskrouter_workflow":   resourceTaskRouterWorkflow(),
-			"twilio_taskrouter_taskqueue":  resourceTaskRouterTaskQueue(),
-			"twilio_taskrouter_activity":   resourceTaskRouterActivity(),
-			"twilio_runtime_service":       resourceRuntimeService(),
-			"twilio_runtime_environment":   resourceRuntimeEnvironment(),
-			"twilio_flex_flow":             resourceFlexFlow(),
+			"twilio_studio_flow_v2": studioV2.ResourceFlows(),
 		},
 	}
 
@@ -51,14 +38,9 @@ func Provider() *schema.Provider {
 	return p
 }
 
-// Config is provided as context to the underlying resources.
-type Config struct {
-	Client *client.Twilio
-}
-
 func providerClient(p *schema.Provider) schema.ConfigureContextFunc {
 	return func(c context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-		config := &Config{
+		config := &twilio.Config{
 			Client: client.NewClient(d.Get("account_sid").(string), d.Get("auth_token").(string)),
 		}
 

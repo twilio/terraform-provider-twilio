b1f80c2fc432fcba10d1d8901f1204925d8dcf3d
shwetharadhakrishna <sradhakrishna@twilio.com>
2021-06-21 10:30:37 -0500

    adding back resourcesMap test

--- twilio/provider_test.go
+++ twilio/provider_test.go
@@ -4,6 +4,9 @@ import (
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/twilio/terraform-provider-twilio/twilio/resources"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -22,11 +25,16 @@ func TestMain(m *testing.M) {
 	resource.TestMain(m)
 }
 
+func TestTwilioResourcesMap(t *testing.T) {
+	twilioResources := resources.NewTwilioResources()
+	assert.NotNil(t, twilioResources.Map["twilio_api_accounts_messages_v2010"])
+}
+
 func testAccPreCheck(t *testing.T) {
 	if v := os.Getenv("TWILIO_ACCOUNT_SID"); v == "" {
-		t.Fatal("ACCOUNT_SID must be set for acceptance tests")
+		t.Fatal("TWILIO_ACCOUNT_SID must be set for acceptance tests")
 	}
 	if v := os.Getenv("TWILIO_AUTH_TOKEN"); v == "" {
-		t.Fatal("AUTH_TOKEN must be set for acceptance tests")
+		t.Fatal("TWILIO_AUTH_TOKEN must be set for acceptance tests")
 	}
 }

e5563361b00f34e6e8bea0027b9cfa5fb17f2214
Sam Harrison <sharrison@twilio.com>
2021-06-24 09:36:25 -0500

    docs: add taskrouter example

---
+++ examples/taskrouter/v1/taskrouter.tf
@@ -0,0 +1,105 @@
+terraform {
+  required_providers {
+    twilio = {
+      source  = "twilio.com/twilio/twilio"
+      version = ">=0.2.0"
+    }
+  }
+}
+
+provider "twilio" {
+  //  account_sid defaults to TWILIO_ACCOUNT_SID env var
+  //  auth_token  defaults to TWILIO_AUTH_TOKEN env var
+}
+
+resource "twilio_taskrouter_workspaces_v1" "demo" {
+  friendly_name = "demoWorkspace"
+}
+
+resource "twilio_taskrouter_workspaces_activities_v1" "offline" {
+  workspace_sid = twilio_taskrouter_workspaces_v1.demo.sid
+  friendly_name = "OfflineCustom"
+  available     = false
+}
+
+resource "twilio_taskrouter_workspaces_activities_v1" "idle" {
+  workspace_sid = twilio_taskrouter_workspaces_v1.demo.sid
+  friendly_name = "Idle"
+  available     = true
+}
+
+resource "twilio_taskrouter_workspaces_activities_v1" "busy" {
+  workspace_sid = twilio_taskrouter_workspaces_v1.demo.sid
+  friendly_name = "Busy"
+  available     = false
+}
+
+resource "twilio_taskrouter_workspaces_activities_v1" "reserved" {
+  workspace_sid = twilio_taskrouter_workspaces_v1.demo.sid
+  friendly_name = "Reserved"
+  available     = false
+}
+
+resource "twilio_taskrouter_workspaces_workers_v1" "alice" {
+  workspace_sid = twilio_taskrouter_workspaces_v1.demo.sid
+  friendly_name = "Alice"
+  activity_sid  = twilio_taskrouter_workspaces_activities_v1.offline.sid
+  attributes = jsonencode({
+    "languages" : [
+      "en",
+      "es"
+    ]
+  })
+}
+
+resource "twilio_taskrouter_workspaces_workers_v1" "bob" {
+  workspace_sid = twilio_taskrouter_workspaces_v1.demo.sid
+  friendly_name = "Bob"
+  activity_sid  = twilio_taskrouter_workspaces_activities_v1.offline.sid
+  attributes = jsonencode({
+    "languages" : [
+      "en"
+    ]
+  })
+}
+
+resource "twilio_taskrouter_workspaces_task_queues_v1" "spanish" {
+  workspace_sid  = twilio_taskrouter_workspaces_v1.demo.sid
+  friendly_name  = "Spanish"
+  target_workers = "languages HAS \"es\""
+}
+
+resource "twilio_taskrouter_workspaces_task_queues_v1" "english" {
+  workspace_sid = twilio_taskrouter_workspaces_v1.demo.sid
+  friendly_name = "English"
+  target_workers = "languages HAS \"en\""
+}
+
+resource "twilio_taskrouter_workspaces_workflows_v1" "flow" {
+  workspace_sid = twilio_taskrouter_workspaces_v1.demo.sid
+  friendly_name = "Flow"
+  configuration = jsonencode({
+    task_routing:{
+      filters:[
+        {
+          filter_friendly_name: "Language - Spanish",
+          expression: "selected_language == \"es\"",
+          targets:[
+            {
+              queue: twilio_taskrouter_workspaces_task_queues_v1.spanish.sid
+            }
+          ]
+        },
+        {
+          filter_friendly_name: "Language - English",
+          expression: "selected_language == \"en\"",
+          targets:[
+            {
+              queue: twilio_taskrouter_workspaces_task_queues_v1.english.sid
+            }
+          ]
+        },
+      ]
+    }
+  })
+}
